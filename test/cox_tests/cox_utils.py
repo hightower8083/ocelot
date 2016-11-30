2312a7694124ce69fc1298f29e4a0c794b085d63
Igor Andriyash <igor.andriyash@gmail.com>
2016-11-30 18:06:08 +0100

    continious spectra beams are on their way

--- test/cox_tests/cox_utils.py
+++ test/cox_tests/cox_utils.py
@@ -12,15 +12,39 @@ def make_beam(bm):
 	parts0[3] = bm['Oy']*np.random.randn(bm['Np'])
 	parts0[4] = bm['Lz']*np.random.randn(bm['Np'])
 	parts0[5] = bm['dE']*np.random.randn(bm['Np'])
-	p_array_init = oclt.ParticleArray()
-	p_array_init.particles = parts0.T.flatten()
-	p_array_init.E = bm['E']
-	p_array_init.s = 0.0
+	p_array = oclt.ParticleArray()
+	p_array.particles = parts0.T.flatten()
+	p_array.E = bm['E']
+	p_array.s = 0.0
 	if 'Q' in bm:
-		p_array_init.q_array = (bm['Q']/bm['Np'])*np.ones(bm['Np'])
+		p_array.q_array = (bm['Q']/bm['Np'])*np.ones(bm['Np'])
 	else:
-		p_array_init.q_array = np.ones(bm['Np'])
-	return p_array_init
+		p_array.q_array = np.ones(bm['Np'])
+	return p_array
+
+def make_beam_contin(bm):
+	p_array = oclt.ParticleArray()
+
+	E1 = bm['E'][0]
+	E2 = bm['E'][1]
+	dE = (E2-E1)/(E2+E1)
+	p_array.E = 0.5*(E2+E1)
+
+	parts0 = np.zeros((6,bm['Np']))
+	parts0[0] = bm['Rx']*np.random.randn(bm['Np'])
+	parts0[1] = bm['Ox']*np.random.randn(bm['Np'])
+	parts0[2] = bm['Ry']*np.random.randn(bm['Np'])
+	parts0[3] = bm['Oy']*np.random.randn(bm['Np'])
+	parts0[4] = bm['Lz']*np.random.randn(bm['Np'])
+	parts0[5] = dE*(2*np.random.rand(bm['Np'])-1)
+
+	p_array.particles = parts0.T.flatten()
+	p_array.s = 0.0
+	if 'Q' in bm:
+		p_array.q_array = (bm['Q']/bm['Np'])*np.ones(bm['Np'])
+	else:
+		p_array.q_array = np.ones(bm['Np'])
+	return p_array
 
 def make_line(Drifts = Drifts,QuadLengths=QuadLengths,QuadGradients=QuadGradients,DipLengths=DipLengths,\
   DipAngles=DipAngles,UndulConfigs=UndulConfigs,cell_keys=cell_keys):
@@ -36,6 +60,14 @@ def make_line(Drifts = Drifts,QuadLengths=QuadLengths,QuadGradients=QuadGradient
 	for key in  ['IMG1','IMG2','IMG4','IMG5',]: LattObjs[key] = oclt.Marker()
 	LattObjs['UNDL'] = oclt.Undulator(Kx=UndulConfigs['Strength'],nperiods=UndulConfigs['NumPeriods'],lperiod=UndulConfigs['Period'])
 	return LattObjs
+
+def damp_particles(p_array, Rx,Ry):
+	Np = p_array.size
+	s0 = p_array.s
+	Rx0, Ry0 = Rx(s0), Ry(s0)
+	indx = np.nonzero((np.abs(p_array.x())<Rx0)*(np.abs(p_array.y())<Ry0))[0]
+	p_array.particles = p_array.particles.reshape((p_array.size(),6))[indx,:].flatten()
+	return p_array, p_array.size()
 
 def make_shot(p_array_init,QAP_T,cell,meth=1):
 	D1,QAP1,D2,QAP2,D3,QAP3,D4 = cell
@@ -87,34 +119,6 @@ def make_shot(p_array_init,QAP_T,cell,meth=1):
 	tw, p_array = oclt.track(lat,p_array,navi,print_progress=False)
 
 	return p_array, np.array([p_array.x().mean()*1e3,p_array.y().mean()*1e3])
-
-def test_bba(QAP_T,QAP_T_fixed,positions,corrections,p_array_init,grad_nom=[0.,0.,0.],meth=1):
-	err = 0.0
-	for i in range(len(positions)):
-		cell = make_cell(corrections[i],grad_nom)
-		p_array, cents = make_shot(p_array_init, np.r_[QAP_T_fixed,QAP_T],cell,meth=meth)
-		err += (cents[0]-positions[i][0])**2+(cents[1]-positions[i][1])**2
-	return err
-
-def test_bba1(QAP_T13,QAP_T_fixed2,positions,corrections,p_array_init,grad_nom=[0.,0.,0.],meth=1):
-	err = 0.0
-	QAP_T13 = QAP_T13.reshape(2,2)
-	for i in range(len(positions)):
-		cell = make_cell(corrections[i],grad_nom)
-
-		arr = np.concatenate((QAP_T13[0][None,:],QAP_T_fixed2[None,:],QAP_T13[1][None,:]),axis=0)
-		p_array, cents = make_shot(p_array_init, arr.flatten(),cell,meth=meth)
-		err += (cents[0]-positions[i][0])**2+(cents[1]-positions[i][1])**2
-	return err
-
-def test_bba2(QAP_T1,QAP_T_fixed23,positions,corrections,p_array_init,grad_nom=[0.,0.,0.],meth=1):
-	err = 0.0
-	for i in range(len(positions)):
-		cell = make_cell(corrections[i],grad_nom)
-		arr = np.concatenate((QAP_T1[None,:],QAP_T_fixed23),axis=0)
-		p_array, cents = make_shot(p_array_init, arr.flatten(),cell,meth=meth)
-		err += (cents[0]-positions[i][0])**2+(cents[1]-positions[i][1])**2
-	return err
 
 def beam_plot(p_array_init,QAPshifts,cell,ax=None,meth=1,**kw):
 	if ax==None: fig,ax = plt.subplots(1,1,**kw)
