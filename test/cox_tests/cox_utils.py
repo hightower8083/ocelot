84edce88399a2b55dbf1693002c5c8313f9bdc5f
hightower8083 <igor.andriyash@gmail.com>
2017-06-07 15:53:52 +0200

    supermatching function is now moved to cox_utils

--- test/cox_tests/cox_utils.py
+++ test/cox_tests/cox_utils.py
@@ -11,6 +11,7 @@ import numpy as np
 import sys
 import ocelot as oclt
 from scipy.constants import m_e,c,e
+from scipy.optimize import fmin
 
 np.set_printoptions(precision=6, suppress=True, linewidth=120)
 
@@ -55,45 +56,6 @@ def sliced_spectrum(Emin,Emax,dg = 0.001,):
 		nrg_sliced.append(e_p)
 		e_m = e_p
 	return np.array(nrg_sliced)
-
-def make_beam_sliced(bm,dg=0.002,div_chirp=None):
-	"""
-	Makes a continuous spectrum beam as a list of slices
-
-	Parameters
-	----------
-	bm : dictionary
-	  beam input parameters as defined for make_beam_contin function
-	dg : float
-	  relative width of the slices
-
-	Returns
-	-------
-	p_arrays : list of oclt.ParticleArray objects
-	  list of the beams
-
-	See Also
-	--------
-	  make_beam and make_beam_contin
-
-	"""
-
-	p_arrays = []
-	E1 = bm['E'][0]
-	E2 = bm['E'][1]
-	nrg_sliced =  sliced_spectrum(E1,E2,dg=dg)
-	nrg_cntrs = 0.5*(nrg_sliced[1:]+nrg_sliced[:-1])
-	Nbeams = nrg_sliced.shape[0]-1
-	part_per_slice = np.round(bm['Np'] \
-	  *(nrg_sliced[1:]-nrg_sliced[:-1]) \
-	  /(nrg_sliced[-1]-nrg_sliced[0])).astype('i')
-	for i in range(Nbeams):
-		beam = oclt.deepcopy(bm)
-		beam['Np'] = part_per_slice[i]
-		beam['Q'] /= Nbeams
-		beam['E'] = (nrg_sliced[i],nrg_sliced[i+1])
-		p_arrays.append(make_beam_contin(beam,div_chirp=div_chirp))
-	return p_arrays
 
 def make_beam(bm):
 	"""
@@ -140,6 +102,46 @@ def make_beam(bm):
 	else:
 		p_array.q_array = np.ones(bm['Np'])
 	return p_array
+
+def make_beam_sliced(bm,dg=0.002,div_chirp=None):
+	"""
+	Makes a continuous spectrum beam as a list of slices
+
+	Parameters
+	----------
+	bm : dictionary
+	  beam input parameters as defined for make_beam_contin function
+	dg : float
+	  relative width of the slices
+
+	Returns
+	-------
+	p_arrays : list of oclt.ParticleArray objects
+	  list of the beams
+
+	See Also
+	--------
+	  make_beam and make_beam_contin
+
+	"""
+
+	p_arrays = []
+	E1 = bm['E'][0]
+	E2 = bm['E'][1]
+	nrg_sliced =  sliced_spectrum(E1,E2,dg=dg)
+	nrg_cntrs = 0.5*(nrg_sliced[1:]+nrg_sliced[:-1])
+	Nbeams = nrg_sliced.shape[0]-1
+	part_per_slice = np.round(bm['Np'] \
+	  *(nrg_sliced[1:]-nrg_sliced[:-1]) \
+	  /(nrg_sliced[-1]-nrg_sliced[0])).astype('i')
+	for i in range(Nbeams):
+		beam = oclt.deepcopy(bm)
+		beam['Np'] = part_per_slice[i]
+		beam['Q'] /= int(bm['Np']/beam['Np'])
+		beam['E'] = (nrg_sliced[i],nrg_sliced[i+1])
+		p_arrays.append(make_beam_contin(beam,div_chirp=div_chirp))
+	return p_arrays
+
 
 def make_beam_contin(bm, div_chirp=None):
 	"""
@@ -606,93 +608,6 @@ def damp_particles(p_array, Rx,Ry):
 	Np = np.int(p_array.size())
 	return p_array, Np
 
-def ocelot_to_chimera(p_arrays,beam,lam0,keep_orig=True,\
-  monochrom=None, select_parts = None):
-	"""
-	Exports the list of ParticleArrays from OCELOT to CHIMERA
-
-	Parameters
-	----------
-	p_arrays: list of oclt.ParticleArray objects
-	  beam represented in the form of slices list
-	beam : chimera.Species object
-	  CHIMERA species obejct to populate with particles
-	lam0: float
-	  normalization length for CHIMERA in meters
-
-	Returns
-	-------
-	beam : chimera.Species object
-		CHIMERA species obejct populated with particles
-	"""
-
-	Np = np.int(np.sum([p_array.size() for p_array in p_arrays]))
-#	g0 = p_array.E/mc2_GeV
-
-	xx = np.hstack(([(p_array.s-p_array.tau())/lam0 for p_array in p_arrays]))
-	yy = np.hstack(([p_array.y()/lam0 for p_array in p_arrays]))
-	zz = np.hstack(([p_array.x()/lam0 for p_array in p_arrays]))
-
-	gg = np.hstack(([(p_array.p()+1)*p_array.E/mc2_GeV for p_array in p_arrays]))
-	oy = np.hstack(([p_array.py() for p_array in p_arrays]))
-	oz = np.hstack(([p_array.px() for p_array in p_arrays]))
-
-	qq = np.hstack(([p_array.q_array*1e12 for p_array in p_arrays]))
-
-	px = np.sqrt( (gg**2-1.)/(1+oy**2+oz**2) )
-	py = px*oy
-	pz = px*oz
-	qq = -qq/(beam.weight2pC*lam0*1e6)
-
-	if monochrom != None:
-		emin,emax = monochrom
-		indx = np.nonzero( (gg*mc2_GeV<emax)*(gg*mc2_GeV>emin)  )[0]
-		xx = xx[indx]
-		yy = yy[indx]
-		zz = zz[indx]
-		px = px[indx]
-		py = py[indx]
-		pz = pz[indx]
-		qq = qq[indx]
-		Np = indx.shape[0]
-
-	if select_parts != None:
-		indx = np.arange(xx.shape[0])
-		np.random.shuffle(indx)
-		indx = indx[:select_parts]
-		xx = xx[indx]
-		yy = yy[indx]
-		zz = zz[indx]
-		px = px[indx]
-		py = py[indx]
-		pz = pz[indx]
-		qq = qq[indx]*Np*1.0/select_parts
-		Np = indx.shape[0]
-
-	gg, oy, oz = None, None, None
-
-	beam.Data['coords'] = np.zeros((3,Np))
-	beam.Data['momenta'] = np.zeros((3,Np))
-	beam.Data['weights'] = np.zeros(Np)
-
-	beam.Data['coords'][0] = xx
-	beam.Data['coords'][1] = yy
-	beam.Data['coords'][2] = zz
-
-	beam.Data['momenta'][0] = px
-	beam.Data['momenta'][1] = py
-	beam.Data['momenta'][2] = pz
-
-	beam.Data['weights'][:] = qq
-
-	beam.Data['coords'][0] -= (beam.Data['coords'][0]*beam.Data['weights']\
-	  ).sum()/(beam.Data['weights']).sum()
-	beam.Data['coords_halfstep'] = beam.Data['coords'].copy()
-	if keep_orig is False:
-		del p_arrays
-
-	return  beam
-
 def print_latt_chars(latt_objs, BeamEnergy):
 	"""
 	Prints some elements of the transport matrices
@@ -731,3 +646,40 @@ def print_R(lat, BeamEnergy):
 
 	print(R_par_str.format(*lat.R.flatten()))
 
+def supermatch_matrix(latt_objs,nrg,varz,MAG=True, printout=True):
+	lat = oclt.MagneticLattice(latt_objs,method=method)
+
+	def error_func(x):
+		for i in range(len(varz)):
+			if varz[i].__class__ == oclt.Quadrupole:
+				varz[i].k1 = x[i]
+				varz[i].transfer_map = lat.method.create_tm(varz[i])
+
+		oclt.cpbd.optics.lattice_transfer_map_RT(lat, nrg)
+
+        
+		err = np.abs( lat.R[0,1])**2 + np.abs(lat.R[2,3] )**2 \
+		    + np.abs(lat.T[1,1,5])**2 + np.abs(lat.T[3,3,5])**2 \
+		    + (np.abs(lat.T[0,1,5])-np.abs(lat.T[2,3,5]))**2
+
+		if MAG is True:
+			err += (np.abs(lat.R[0,0])-np.abs(lat.R[2,2]))**2
+		elif MAG is not True and MAG is not None:
+			err += (np.abs(lat.R[0,0])-MAG[0])**2 + (np.abs(lat.R[2,2])-MAG[1])**2
+
+		return err
+
+	x = [0.0] * len(varz)
+
+	for i in range(len(varz)):
+		if varz[i].__class__ == oclt.Quadrupole:
+			x[i] = varz[i].k1
+
+	if printout: print("initial value: x = ", x)
+	fmin(error_func, x, xtol=1e-9, maxiter=20000, maxfun=20000,disp=printout)
+    
+	for i in range(len(varz)):
+		if varz[i].__class__ == oclt.Quadrupole:
+			x[i] = varz[i].k1
+	if printout: print("final value: x = ", x)
+	return x
